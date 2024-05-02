f27353cee2601485a8eb975bc33d3d218d21dd08
Michal <michalgrzegorczyk04@gmail.com>
2024-05-02 14:17:11 +0200

    dom element selection

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,27 @@
-// Code goes here!
+/** @format */
+
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
