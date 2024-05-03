5b9b36afd7491e064cf52ba27ce284ead7d0ecd1
Michal <michalgrzegorczyk04@gmail.com>
2024-05-03 18:46:30 +0200

    add ts support with ts-loader package

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,23 @@
 
 const path = require("path");
 
-module.export = {
+module.exports = {
   entry: "./src/app.ts",
   output: {
-    filename: "bundle.ts",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
   },
 };
