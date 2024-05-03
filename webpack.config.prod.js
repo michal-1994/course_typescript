6e1ebd7177e270ecd317178d94332c2f46940987
Michal <michalgrzegorczyk04@gmail.com>
2024-05-03 18:59:16 +0200

    add production workflow

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,23 +1,17 @@
 /** @format */
 
 const path = require("path");
+const CleanPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/app.ts",
-  devServer: {
-    static: [
-      {
-        directory: path.join(__dirname),
-      },
-    ],
-  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
     publicPath: "/dist/",
   },
-  devtool: "inline-source-map",
+  devtool: "none",
   module: {
     rules: [
       {
@@ -30,4 +24,5 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".js"],
   },
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
 };
